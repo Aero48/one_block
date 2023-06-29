25c76dc27b873b50bce21467838ba4a5b51e1835
Aero48 <56200404+Aero48@users.noreply.github.com>
2023-06-29 14:14:00 -0400

    further improvements

--- js/data/playerItems.js
+++ js/data/playerItems.js
@@ -7,68 +7,8 @@ export function addTool(itemStack, idx){
 }
 
 export const items = [
-    {
-        name: "Gravel",
-        amount: 40
-    },
-    {
-        name: "Oak Log",
-        amount: 40
-    },
-    {
-        name: "Raw Copper",
-        amount: 40
-    }
+    
 ];
 export const tools = [
-    {
-        name: "Bronze Pickaxe",
-        image: "../images/pickaxe-svgrepo-com.svg",
-        color: "filter: invert(46%) sepia(100%) saturate(392%) hue-rotate(3deg) brightness(97%) contrast(82%);",
-        group: "tool",
-        type: "pickaxe",
-        power: 0.4,
-        maxDurability: 60,
-        durability: 60
-    },
-    {
-        name: "Bronze Axe",
-        image: "../images/axe-tool-construction-svgrepo-com.svg",
-        color: "filter: invert(46%) sepia(100%) saturate(392%) hue-rotate(3deg) brightness(97%) contrast(82%);",
-        group: "tool",
-        type: "axe",
-        power: 0.4,
-        maxDurability: 60,
-        durability: 60
-    },
-    {
-        name: "Bronze Shovel",
-        image: "../images/shovel-svgrepo-com.svg",
-        color: "filter: invert(46%) sepia(100%) saturate(392%) hue-rotate(3deg) brightness(97%) contrast(82%);",
-        group: "tool",
-        type: "shovel",
-        power: 0.4,
-        maxDurability: 60,
-        durability: 60
-    },
-    {
-        name: "Brick Furnace",
-        image: "../images/furnace-svgrepo-com.svg",
-        color: "filter: invert(9%) sepia(77%) saturate(3803%) hue-rotate(336deg) brightness(84%) contrast(102%);",
-        group: "tool",
-        type: "furnace",
-        power: 2,
-        maxDurability: 20,
-        durability: 20
-    },
-    {
-        name: "Standard Sieve",
-        image: "../images/table-svgrepo-com.svg",
-        color: "filter: invert(45%) sepia(49%) saturate(1053%) hue-rotate(340deg) brightness(91%) contrast(81%);",
-        group: "tool",
-        type: "sieve",
-        power: 2,
-        maxDurability: 30,
-        durability: 30
-    },
+    
 ];
